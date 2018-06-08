0c2d4806e17c8d768ad8be24aa449ab26fa991a5
Min RK <benjaminrk@gmail.com>
2018-06-08 14:09:51 +0200

    add test file

---
+++ 2018-04-20-tests/test.py
@@ -0,0 +1,18 @@
+def mode(data):
+    counts = {}
+    for number in data:
+        if number not in counts:
+            counts[number] = 0
+        counts[number] = counts[number] + 1
+
+    max_number = None
+    max_count = 0
+    for number, count in counts.items():
+        if count > max_count:
+            max_number = number
+            max_count = count
+    return max_number
+
+def test_simple_list():
+    data = [1, 2, 3, 4, 5, 2, 3, 3]
+    assert mode(data) == 3
